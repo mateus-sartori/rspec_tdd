0187da7ff4af49eb5f72cbe9e2849402501da3f1
Mateus <mateus-sartori@hotmail.com>
2021-09-23 21:45:40 -0400

    Initial commit

---
+++ spec/calculator/calculator_spec.rb
@@ -0,0 +1,29 @@
+require 'calculator'
+
+# subject = classe que esta sendo descrita (Calculator)
+
+describe Calculator, 'Sobre a Calculadora' do
+  # subject explicito
+  # subject(:calc) { Calculator.new } # renomeia para ser usado como calc
+
+  # subject implicito
+  subject(:calc) { described_class.new } # renomeia para ser usado como calc
+  context '#sum' do
+    it 'with positive numbers' do
+      result = calc.sum(85, 9)
+      expect(result).to eq(result)
+    end
+
+    it 'with negative and positive numbers' do
+      result = calc.sum(-5, 7)
+      expect(result).to eq(result)
+    end
+
+    it 'with negative numbers' do
+      result = calc.sum(-5, -7)
+      expect(result).to eq(result)
+    end
+
+    xit 'with numbers'
+  end
+end
